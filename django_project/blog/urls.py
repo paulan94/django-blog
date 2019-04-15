0fb12e149fbea3eb4f5076b2463b01368967cd8c
paulan <paulan94@gmail.com>
2019-04-15 16:12:25 -0700

    added pagination

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name= 'blog-home'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name= 'user-posts'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name= 'post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name= 'post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name= 'post-update'),
