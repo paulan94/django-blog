11eb43edc63eb2cffba5401110cfecc86d6417eb
paulan <paulan94@gmail.com>
2019-04-17 17:11:21 -0700

    finished tuts

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -12,11 +12,11 @@ class Profile(models.Model):
 		return f'{self.user.username} Profile'
 
 	def save(self):
-		super.save()
+		super().save()
 
 		img = Image.open(self.image.path)
 
 		if img.height > 300 or img.width > 300:
 			output_size = (300,300)
 			img.thumbnail(output_size)
-			img.save()
+			img.save(self.image.path)
